8aa90d2006a6d2b2abfa0bcfd85ae31c457bc19f
Saim Islam <saim.islam698@gmail.com>
2024-06-29 23:38:41 +0600

    Update predatory_credit_card.py

--- predatory_credit_card.py
+++ predatory_credit_card.py
@@ -1,1 +1,18 @@
+import credit_card
+#Inheritance
+class PredatoryCreditCard(credit_card.CreditCard):
+  #Add a new field to subclass
+  def __init__(self, bank, client, account, limit, apr):
+    super().__init__(bank, client, account, limit)
+    self._apr = apr
+    #you can add more field
+  
+  #Override the function charge(price)
+  def charge(self, price):
+    success = super().charge(price):
+ 
+  #you can create new method here, no special syntax is needed
+  def subliminary(self,sub_name):
+    self.sub_name = sub_name
+    return f"{self.sub_name} is added as Subliminary"
 
